48190f35c89784ccf15b37722dc973c923a18106
Shantam <shantamconfidential@gmail.com>
2026-07-13 18:43:38 -0700

    feat: build a graph by adding assistant node and start and end edges , compile the graph , send a human message and show it

--- backend/agent.py
+++ backend/agent.py
@@ -1,21 +1,22 @@
 from langchain.chat_models import init_chat_model
 from typing import TypedDict
 from dotenv import load_dotenv
+from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import HumanMessage
+
 load_dotenv()
 
-
+#initialize the chat model client
 model = init_chat_model(
   "gpt-4o-mini",
    temperature = 0 
 )
 
 #initial assistant state 
-
 class AssistantState(TypedDict):
   messages: list
 
 #initial assistant node
-
 def assistant_node(state: AssistantState):
   """simple llm assistant"""
 
@@ -25,6 +26,27 @@ def assistant_node(state: AssistantState):
   return{
     "messages":[response]
   }
+
+#create the graph(add state)
+graph_builder = StateGraph(AssistantState)
+
+#Add nodes
+graph_builder.add_node("assistant", assistant_node)
+
+#define graph flow
+graph_builder.add_edge(START, "assistant")
+graph_builder.add_edge("assistant", END)
+
+#compile the graph 
+assistant_graph = graph_builder.compile()
+
+#test the graph 
+result = assistant_graph.invoke({
+  "messages":[HumanMessage(content = "Hello, who are you")]
+})
+
+print(result["messages"][-1].content)
+
 
 
 
